f44621b7fa6fb80b1373844b1bd10b42a263e4b6
Jesus Diez <jesussebastiandiezplasencia@gmail.com>
2025-08-08 01:06:54 -0500

    feat: add ag-grid packages and configure in app
    Add ag-grid-angular and ag-grid-community packages to enable data grid functionality
    Register ag-grid modules and set default theme in app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,16 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
+import { AllCommunityModule, ModuleRegistry, provideGlobalGridOptions } from 'ag-grid-community'; 
+
+ModuleRegistry.registerModules([AllCommunityModule]);
+provideGlobalGridOptions({ theme: 'legacy' })
 
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideZoneChangeDetection({ eventCoalescing: true }),
 		provideRouter(routes),
 		provideHttpClient(),
-		provideAnimations()
+		provideAnimations(),
 	]
 };
